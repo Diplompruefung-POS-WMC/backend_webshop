6a701c8d79e5b2cc09045e1bce782c10df4e217f
sa-bri-na <huber_sabrina@me.com>
2024-09-04 08:54:17 +0200

    add GET requests

--- src/main/java/com/example/backend_webshop/controller/WebshopRestController.java
+++ src/main/java/com/example/backend_webshop/controller/WebshopRestController.java
@@ -1,6 +1,7 @@
 package com.example.backend_webshop.controller;
 
 import com.example.backend_webshop.model.Category;
+import com.example.backend_webshop.model.Product;
 import com.example.backend_webshop.repository.CategoryRepository;
 import com.example.backend_webshop.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
@@ -8,7 +9,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -26,5 +29,26 @@ public class WebshopRestController {
         return new ResponseEntity<>(categories, HttpStatus.OK);
     }
 
+    @GetMapping("/categories/{categoryId}/products")
+    public ResponseEntity<List<Product>> getProductsFromCategory(
+            @PathVariable Long categoryId
+    ){
+        Category category = categoryRepository.findById(categoryId).orElseThrow(() ->
+                new ResponseStatusException(HttpStatus.NOT_FOUND, "Error categoryId not exists"));
+
+        List<Product> products = category.getProducts();
+
+        return new ResponseEntity<>(products, HttpStatus.OK);
+    }
+
+    @GetMapping("/product/{productId}")
+    public ResponseEntity<Product> getProduct(
+            @PathVariable Long productId
+    ){
+        Product product  = productRepository.findById(productId).orElseThrow(() ->
+            new ResponseStatusException(HttpStatus.NOT_FOUND, "Error productId not exists"));
+
+        return new ResponseEntity<>(product, HttpStatus.OK);
+    }
 
 }
