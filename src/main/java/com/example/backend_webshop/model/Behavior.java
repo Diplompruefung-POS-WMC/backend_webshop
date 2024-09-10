0e0f86068429e1a9a671ff246532615c7332319b
sa-bri-na <huber_sabrina@me.com>
2024-09-10 17:26:02 +0200

    Add click count

---
+++ src/main/java/com/example/backend_webshop/model/Behavior.java
@@ -0,0 +1,21 @@
+package com.example.backend_webshop.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Behavior {
+    private Integer clickCount;
+    private Date date;
+
+    public void updateClickCount(){
+        this.clickCount++;
+    }
+}
--- src/main/java/com/example/backend_webshop/observer/FileLogger.java
+++ src/main/java/com/example/backend_webshop/observer/FileLogger.java
@@ -1,14 +1,51 @@
 package com.example.backend_webshop.observer;
 
 
+import com.example.backend_webshop.model.Behavior;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
 
 @Slf4j
 @Component
 public class FileLogger implements ObserverLogger{
+    private static HashMap<Long, Behavior> behaviorHashMap = new HashMap<>();
+    private ObjectMapper objectMapper = new ObjectMapper()
+            .registerModule(new JavaTimeModule())
+            .setDateFormat(new SimpleDateFormat("yyyy-MM-dd hh:mm"));
+    @PostConstruct
+    public void loadBehaviorData(){
+        try {
+            behaviorHashMap = objectMapper.readValue(
+                    getClass().getResourceAsStream("/behavior.json"),
+                    new TypeReference<>() {}
+            );
+        } catch (IOException e) {
+            log.error("Error loading behavior data from json: {}", e.getMessage());
+        }
+    }
+
     @Override
     public void update(Long productId) {
-        log.info("Filelogger");
+        Behavior productBehavior = behaviorHashMap.getOrDefault(productId, new Behavior(0, new Date()));
+
+        productBehavior.updateClickCount();
+        behaviorHashMap.put(productId, productBehavior);
+
+        try {
+            objectMapper.writeValue(new File("productClicks.json"), behaviorHashMap);
+        } catch (IOException e) {
+            log.error("Error writing behavior.json", e);
+        }
     }
 }
